709c50157083e9b2b8c4590036792927dee01265
Melissa Weinstein <mel.weinstein31@gmail.com>
2019-06-30 18:35:34 +0300

    updated mood controller

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -1,7 +1,20 @@
 class MoodsController < ApplicationController
   def new
+    @mood = Mood.new
   end
 
   def create
+    @mood = Mood.new(mood_params)
+    if @mood.save
+      redirect_to days_path
+    else
+      render :new
+    end
   end
+
+  private
+
+    def mood_params
+      params.require(:mood).permit(:feeling)
+    end
 end
