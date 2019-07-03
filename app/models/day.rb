0c1365215e176f173596bae47e13bdeec59ecb66
gabrielle simon <gabriellesimon23@gmail.com>
2019-07-03 15:45:33 +0300

    show page styled

--- app/models/day.rb
+++ app/models/day.rb
@@ -7,6 +7,12 @@ class Day < ApplicationRecord
 
   HOURS = (0..12).to_a
 
-
-
+  def all_events_today
+    event_day = []
+    self.exercises.each{ |ex| event_day << ex }
+    self.moods.each{ |mo| event_day << mo }
+    self.pains.each{ |pain| event_day << pain }
+    self.sleeps.each{ |sl| event_day << sl }
+    event_day
+  end
 end
