c16c2edf060dd447e6f54db86fb41b7f1d266822
gabrielle simon <gabriellesimon23@gmail.com>
2019-07-03 19:26:17 +0300

    ajax added missing empty square

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -5,8 +5,18 @@ class DaysController < ApplicationController
     @pains = Pain.joins(:day).where(days: { user: current_user })
     @sleeps = Sleep.joins(:day).where(days: { user: current_user })
     @events = @moods + @exercises + @pains + @sleeps
-
     @days = policy_scope(Day)
+    if params[:day]
+      @day = Day.find(params[:day])
+    else
+     @day = Day.find_by(date: Date.today)
+      #render '../views/shared/show_events'
+ end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def show
