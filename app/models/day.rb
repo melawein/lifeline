407f5ce2b8fa1162a71cfd265be9e9dad7d51fcd
Melissa Weinstein <mel.weinstein31@gmail.com>
2019-07-05 13:40:34 +0300

    made fixes to calendar and some design changes

--- app/models/day.rb
+++ app/models/day.rb
@@ -7,12 +7,29 @@ class Day < ApplicationRecord
 
   HOURS = (0..12).to_a
 
-  def all_events_today
+
+  def all_events_today(user)
     event_day = []
-    self.exercises.each{ |ex| event_day << ex }
-    self.moods.each{ |mo| event_day << mo }
-    self.pains.each{ |pain| event_day << pain }
-    self.sleeps.each{ |sl| event_day << sl }
+    self.exercises.each do |ex|
+      if ex.day.user == user
+        event_day << ex
+      end
+    end
+    self.moods.each do |mo|
+      if mo.day.user == user
+        event_day << mo
+      end
+    end
+    self.pains.each do |pain|
+      if pain.day.user == user
+        event_day << pain
+      end
+    end
+    self.sleeps.each do |sl|
+      if sl.day.user == user
+        event_day << sl
+      end
+    end
     event_day
   end
 end
