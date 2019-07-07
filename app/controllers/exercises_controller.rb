c281ae4c81b98a55de1b8e17b395e07ad9cc65e1
Melissa Weinstein <mel.weinstein31@gmail.com>
2019-07-07 10:02:59 +0300

    fix exercise wheel

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -5,36 +5,30 @@ class ExercisesController < ApplicationController
   end
 
   def create
-
-    time = params[:exercise][:time]
+    # time = params[:exercise][:time]
 
     date = Date.new(params["day"]["date(1i)"].to_i, params["day"]["date(2i)"].to_i, params["day"]["date(3i)"].to_i)
     day = Day.find_by(date: date, user: current_user)
     day = Day.create(date: date, user: current_user) if day.nil?
 
-    exercise = Exercise.new(exercise_params)
-    exercise.day = day
-    authorize exercise
+    @exercise = Exercise.new(exercise_params)
+    @exercise.day = day
+    authorize @exercise
 
-    if exercise.save
+    if @exercise.save
       redirect_to days_path
     else
       render :new
     end
+  end
 
-    def destroy
+  def destroy
 
-      @exercise = Exercise.find(params[:id])
-      day = @exercise.day
-      authorize @exercise
-      @exercise.destroy
-      redirect_to days_path
-
-      respond_to do |format|
-        format.html
-        format.js
-      end
-    end
+    @exercise = Exercise.find(params[:id])
+    day = @exercise.day
+    authorize @exercise
+    @exercise.destroy
+    redirect_to days_path
   end
 
   private
